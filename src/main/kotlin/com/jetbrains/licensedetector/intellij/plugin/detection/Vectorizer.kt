1bf9172bc574f3a4ac6a52ebf6f05bf0d354b40a
Ivan Kuznetsov <tikerlade@gmail.com>
2021-02-18 14:04:30 +0300

    Documented Vectorizer class. Clean code.

--- src/main/kotlin/com/jetbrains/licensedetector/intellij/plugin/detection/Vectorizer.kt
+++ src/main/kotlin/com/jetbrains/licensedetector/intellij/plugin/detection/Vectorizer.kt
@@ -2,9 +2,19 @@ package com.jetbrains.licensedetector.intellij.plugin.detection
 
 import java.io.File
 
-class Vectorizer constructor(val vocabulary: List<String>){
+/**
+ * Supportive class for Detector.
+ * Transform text into vector of dimension = size of vocabulary size.
+ * @property vocabulary vocabulary which use to vectorize text.
+ */
+class Vectorizer(val vocabulary: List<String>){
     val vector_dim = vocabulary.size
 
+    /**
+     * Vectorize (counting vectorization) given text into vector_dim size vector.
+     * @param text text to be vectorized.
+     * @return IntArray with vector_dim size (vector).
+     */
     fun vectorize(text: String): IntArray {
         // Initialize empty vector for text
         val text_vector = IntArray(vector_dim)
@@ -29,23 +39,34 @@ class Vectorizer constructor(val vocabulary: List<String>){
         return text_vector
     }
 
+    /**
+     * Vectorize (counting vectorization) text from file into vector_dim size vector.
+     * @param file file from which text to be vectorized.
+     * @return IntArray with vector_dim size (vector).
+     */
     fun vectorize(file: File): IntArray {
         return vectorize(file.readText())
     }
 
+    /**
+     * Detects index of given feature in vocabulary.
+     * @param feature feature which index to be found.
+     * @return index inside
+     */
     fun getFeatureIndex(feature: String): Int {
-        var index = -1
-
         for (i in 1..vector_dim) {
             if (vocabulary[i].equals(feature)) {
-                index = i
-                break
+                return i
             }
         }
-
-        return index
+        return -1
     }
 
+    /**
+     * Finds which feature is located in given index inside vocabulary.
+     * @param index index of feature to be extracted.
+     * @return feature which is located in given index.
+     */
     fun getFeature(index: Int): String {
         return vocabulary[index]
     }
